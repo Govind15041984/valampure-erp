f17750656d4e15ecf77acc3053fefb92bea4dfb3
Govind15041984 <govindraaj.s@gmail.com>
2026-03-20 23:07:40 +0530

    Update Flutter baseUrl to Render production URL

--- frontend/valampure_app/lib/api/config_api.dart
+++ frontend/valampure_app/lib/api/config_api.dart
@@ -1,2 +1,3 @@
-const String kBaseUrl = 'http://192.168.18.150:8000';
+//const String kBaseUrl = 'http://192.168.18.150:8000';
 //const String kBaseUrl = 'https://chitpro.onrender.com';
+const String kBaseUrl = 'https://valampure-erp-backend.onrender.com';
